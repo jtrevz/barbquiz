a16a5e2d3af4f03c3305aea502bd95f3083611e0
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-03-29 15:31:01 -0500

    added html, css, js files along with html and some css code

---
+++ script.js
@@ -0,0 +1,74 @@
+var barbQuestions = [
+  {
+    q:
+      "When Queen Nicki was calling all Barbz, where was she making them report to?",
+    a: "A Concert",
+    b: "Her Live",
+    c: "The Conference Room",
+    d: "Trisha's House",
+  },
+  {
+    q:
+      "She's huge, but she's so beautiful. She's a mammoth of course 🙄. Who is she?",
+    a: "Sid",
+    b: "Steffie",
+    c: "Trisha",
+    d: "Scrat",
+  },
+  {
+    q: "You b*tches can't even spell ______.",
+    a: "Prague",
+    b: "Supercalifragilisticexpialidocious",
+    c: "Barbz",
+    d: "Csechoslovakia",
+  },
+  {
+    q: "Did you guys see my ____ and my ______?",
+    a: "🐙  🌸",
+    b: "🔥  💰",
+    c: "⏰  👗",
+    d: "🐱  🍑",
+  },
+  {
+    q: "Queen Nicki hates doing shit and not _______?",
+    a: "Finishing it",
+    b: "Perfecting it",
+    c: "Showing off",
+    d: "Stopping",
+  },
+  {
+    q: "Why are they there then? What is they???",
+    a: "Barbz",
+    b: "Fans",
+    c: "Gumbawlls",
+    d: "Cats",
+  },
+  {
+    q: "Who's not valid?",
+    a: "Barbz",
+    b: "Nicki Lewinski",
+    c: "Roman Nicki",
+    d: "Broke people who can't pay their mother effin bills",
+  },
+  {
+    q: "Who should never give you fashion advice?",
+    a: "Jean vest wearers",
+    b: "Anna Wintour",
+    c: "Queen Nicki",
+    d: "Jenny",
+  },
+  {
+    q: "Who should never laugh?",
+    a: "Barbz",
+    b: "Broke people",
+    c: "Idiots",
+    d: "Trisha",
+  },
+];
+var quizBox = document.getElementById("quiz");
+var resultsBox = document.getElementById("results");
+var submit = document.getElementById("submit");
+
+function quiz() {
+  for (i = 0; i < barbQuestions.length; i++) {}
+}
